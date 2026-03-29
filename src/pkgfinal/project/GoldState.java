a88d9f8da2c7b38cdadbb46f85af01c3b4a82814
smokahal <smokahal@torontomu.ca>
2026-03-29 16:40:02 -0400

    Update GoldState.java

--- src/pkgfinal/project/GoldState.java
+++ src/pkgfinal/project/GoldState.java
@@ -10,7 +10,22 @@ public class GoldState implements StatusState {
     public void handlePurchase(Customer customer, double amount) {
         customer.addPoints((int)(amount * 10));
     }
-
+    
+    @Override
+    public double redeemPoints(Customer customer, double totalCost) {
+        double discount = customer.getPoints() / 100.0; // 100 pts = $1
+        if (discount >= totalCost) {
+            customer.addPoints(-(int)(totalCost * 100));
+            totalCost = 0.0;
+        } else {
+            customer.addPoints(-customer.getPoints());
+            totalCost -= discount;
+        }
+        if (customer.getPoints() < 1000) {
+            customer.setStatus(new SilverState()); // transition down
+        }
+        return totalCost;
+    }
     @Override
     public String getStatusName() {
         return "Gold";
